51d4840ed077204a51475f58aa6baf44f7d179b8
KarenBench <githubk@benchmarkcomputing.com>
2024-02-25 07:06:46 -0500

    fixed typo in batch file

--- RollBack-MPHMain.sh
+++ RollBack-MPHMain.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd /alta/Main
-rm -rv !("appsettings.json"|"UseNewDeploy.sh"|"staging"|"Logs")
-cp -rp ../safety-Main/* ./
+cd /alta/MPHMain
+rm -rv !("appsettings.json"|"UseNewDeploy-MPHMain.sh"|"staging"|"Logs")
+cp -rp ../safety-MPHMain/* ./
 chmod +x *.sh
-./Startme-Main.sh
 ./ShowRunning.sh
 
