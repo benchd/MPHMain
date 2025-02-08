186f728a54be4089b21796b04db62bffb38ed8e4
David Bench <github@benchmarkcomputing.com>
2025-02-08 14:32:24 -0500

    Tried again, and this time it did login locally

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@
             <!-- KLB  target="_blank" href="https://myprohelper.com/login">Login</a></li>  -->
           <li><a class="getstarted scrollto ms-0 signin" v-tooltip
               title="Easy connection to MyProHelper application after your company is setup for a trial or a subscription" 
-              target="_blank" href="http://localhost:5173/login">Login</a></li>
+              target="_blank" href="http://localhost:8080/login">Login</a></li>
         </ul>
         <i :class="navbar_mobile ? 'bi bi-x mobile-nav-toggle' : 'bi bi-list mobile-nav-toggle'" @click="showMenu"></i>
       </nav>
@@ -102,7 +102,7 @@ export default {
     return {
       navbar_mobile: false,
       modal: null,
-      maineLoginUrl : "http://localhost:5173/login",
+      maineLoginUrl : "http://localhost:8080/login",
 //klb      maineLoginUrl : "https://myprohelper.com/login",
      // privacyUrl: "https://myprohelper.com:5005/api/PrivacyPolicy", // this was here but doesn't work
       privacyUrl: "http://localhost:5011/api/PrivacyPolicy",
