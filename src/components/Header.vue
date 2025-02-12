3c4b0bca2c33cf454a7983e92fd000b5d4929d18
KarenBench <githubk@benchmarkcomputing.com>
2025-02-12 07:54:50 -0500

    fix in Header.vue to go to login page on server.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,9 +22,10 @@
           <li><a class="start_free scrollto" href="javascript://" @click="handleStartFreeTrial">Start Free Trial</a>
           </li>
             <!-- KLB  target="_blank" href="https://myprohelper.com/login">Login</a></li>  -->
-          <li><a class="getstarted scrollto ms-0 signin" v-tooltip
+            <!-- target="_blank" href="http://localhost:8080/login">Login</a></li>   -->
+            <li><a class="getstarted scrollto ms-0 signin" v-tooltip
               title="Easy connection to MyProHelper application after your company is setup for a trial or a subscription" 
-              target="_blank" href="http://localhost:8080/login">Login</a></li>
+              target="_blank" href="https://myprohelper.com/login">Login</a></li>
         </ul>
         <i :class="navbar_mobile ? 'bi bi-x mobile-nav-toggle' : 'bi bi-list mobile-nav-toggle'" @click="showMenu"></i>
       </nav>
